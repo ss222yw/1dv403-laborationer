5b8c1c41e1f3d9e548100ec8be0c19af7ba3609b
sahib <ss222yw@student.lnu.se>
2013-11-20 14:00:46 -0500

    klar med sista uppgift

--- 1-vandalen/tests/vandalen.js
+++ 1-vandalen/tests/vandalen.js
@@ -1,5 +1,5 @@
 test( "Datatyper", function() {
-  var indata = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
+  var indata = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
 
   ok( typeof makePerson(indata) == "object" , "Funktionen returnerar ett objekt." );
   
@@ -12,8 +12,8 @@ test( "Datatyper", function() {
 
 test( "Namnhantering (names)", function() {
 
-  var indata = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
-  var indata2 = [{name: "Ö", born: "1976-02-23"}, {name: "Å", born: "1977-12-12"},  {name: "Ä", born: "1967-06-02"}];
+  var indata = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
+  var indata2 = [{name: "Ö", age: 37}, {name: "Å", age: 36},  {name: "Ä", age: 46}];
 
 
   equal( makePerson(indata).names, "Johan Leitet, John Häggerud, Mats Loock" , "Namn returneras sorterat" );
@@ -23,7 +23,7 @@ test( "Namnhantering (names)", function() {
 
 test( "Åldershantering (maxAge, minAge, aveargeAge)", function() {
 
-  var indata2 = [{name: "John Häggerud", born: "1976-02-23"}, {name: "Johan Leitet", born: "1977-12-12"},  {name: "Mats Loock", born: "1967-06-02"}];
+  var indata2 = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
   var indata = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36},  {name: "Mats Loock", age: 46}];
   
   equal( makePerson(indata).maxAge, 46, "maxAge är 46" );
--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,52 @@
 "use strict";
 
 var makePerson = function(persArr){
+var result = {};
+var ages = [];
+var names = [];
+var sum;
+	// Din kod här...
+
+ages = persArr.map(function(person){
+   return  person.age;
+});
 
 
 
+result.maxAge = ages.reduce(function(prevAge, age){
+    return Math.max(prevAge, age);
+});
 
+result.minAge = ages.reduce(function(prevAge, age) {
+    return Math.min(prevAge, age);
+});
+
+sum = ages.reduce(function(prevAge, age){
+    return prevAge + age;
+});
+
+names = persArr.map(function(person) {
+    return person.name;
+});
+
+names.sort(function(a, b){
+   return a.localeCompare(b); 
+});
+
+result.names = names.reduce(function(prevName, name) {
+    return prevName + ", " + name;
+});
+
+result.averageAge = Math.round(sum/3);
+
+
+console.log(result.names);
+console.log(ages);
+console.log(sum);
+console.log(result.maxAge);
+
+    
+return result;
 };
+
 
