bbb10b341b92d3b8f0e7f59ddfff882d8f167a40
sahib <ss222yw@student.lnu.se>
2013-12-01 15:03:10 -0500

    Lagt till mer kod i message.js

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -32,7 +32,42 @@ Message.prototype.getDateText = function() {
 	var hours = time.getHours();
 	var minutes = time.getMinutes();
 	var seconds = time.getSeconds();
-	var getDateText = hours+":"+minutes+":"+seconds;
-	return getDateText;
-
+	// Ifall timmen är mindre än 10 lägg till en 0 innan siffran tex 9 blir 09.
+if(hours < 10){
+		hours = "0" + hours;
+	}
+	// Ifall minuten är lägre än 10 , lägg till en 0 innan nummret tex: 8 blir 08.
+	if (minutes < 10){
+		minutes = "0" + minutes;
+	}
+	// Ifall suknden är mindre än 10 , lägg till en 0 framför siffran tex: 7 blir 07.
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	var dateText = hours +":"+ minutes +":"+seconds;
+	return dateText;
+};
+//Returnerar den fulla tiden.
+Message.prototype.getTimeStamp = function(){
+	var time = this.getDate();
+	var year = time.getFullYear();
+	var month = time.getMonth();
+	var day = time.getDate();
+	// 0 är lika med januari osv tills 11 är lika med december.
+	//I den här fallet ändrar vi månaderna siffror till ord, så det ser ut som det finns i själva labben.
+	if(month === 0){month = "januari";}
+	if(month === 1){month = "februari";}
+	if(month === 2){month = "mars";}
+	if(month === 3){month = "april";}
+	if(month === 4){month = "maj";}
+	if(month === 5){month = "juni";}
+	if(month === 6){month = "juli";}
+	if(month === 7){month = "augusti";}
+	if(month === 8){month = "september";}
+	if(month === 9){month = "oktober";}
+	if(month === 10){month = "november";}
+	if(month === 11){month = "december";}
+	
+	var timeStamp = "Inlägget skapades den " + day + " "  + month + " " + year + " klockan " + this.getDateText();
+	return timeStamp;
 };
