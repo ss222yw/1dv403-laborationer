532f733064b98fb22fe20e60d0c3dda31e107b20
sahib <ss222yw@student.lnu.se>
2013-12-08 10:29:42 -0500

    Genererat upp tabbelen som inhåller bilderna

--- 3-gameon/memory/memory.js
+++ 3-gameon/memory/memory.js
@@ -7,13 +7,53 @@ var rows = 4;
 var Memory = {
     //En egenskap som kommer att referera till den utslumpade arrayen.
     randomArray:[],
+    array: [],
     //Skapar en init-metod och koppla denna till sidans onload-event.
      init: function(){
          // I init-metoden anropar arrayslumpsmetoden och sparar resultatet i egenskapen som skapade i 4an.
         Memory.randomArray =  RandomGenerator.getPictureArray(rows, cols)
         //Test för att se om det går att skriv ut arrayen.
         console.log(Memory.randomArray);
+        //Anropar generatorUl.
+        Memory.generatorUl();
     },
+    
+    //Generera upp tabellen ul som inhåller bilderna!
+    generatorUl: function(){
+        var ul = document.createElement("ul");
+        var tr = null;
+        var pics = null;
+        var td = null;
+        var li = null;
+        var a = null;
+        // Varivale som räknar.
+        var count = 0;
+        // lägger ul i divtaggen content.
+        document.getElementById("content") .appendChild("ul");
+        
+        for (var row = 0; row < rows; ++rows ){
+            tr = document.createElement("tr");
+            ul.appendChild("tr");
+            
+            for(var col = 0; col < cols; ++cols){
+                 td = document.createElement("td");
+                tr.appendChild("td");
+                //Skappar bilderna.
+                pics = document.createElement("img");
+                pics.setAttribute("src", "pics/0.png");
+                // Modifierar koden så att varje bild kapslas in i en a-länk och koppla ett onclick-event till a-taggen
+                li = document.createElement("li");
+                a = document.createElement("a");
+                a.href = "#";
+                li.appendChild("pics");
+                td.appendChild("li");
+                // Räknar upp.
+                count++;
+                
+            }
+        }
+        
+    }
    
 };
 
