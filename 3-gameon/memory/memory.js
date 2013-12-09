4e7a1488404d105810257a0ef0b5360845bea0c3
sahib <ss222yw@student.lnu.se>
2013-12-09 06:49:00 -0500

    fixat lite fel plus att skapat check function

--- 3-gameon/memory/memory.js
+++ 3-gameon/memory/memory.js
@@ -8,6 +8,11 @@ var Memory = {
     //En egenskap som kommer att referera till den utslumpade arrayen.
     randomArray:[],
     array: [],
+    // Räknare för antal försök.
+    countTrying: 0,
+    // Räknar par
+    countPair: 0,
+    
     //Skapar en init-metod och koppla denna till sidans onload-event.
      init: function(){
          // I init-metoden anropar arrayslumpsmetoden och sparar resultatet i egenskapen som skapade i 4an.
@@ -44,9 +49,15 @@ var Memory = {
                 // Modifierar koden så att varje bild kapslas in i en a-länk och koppla ett onclick-event till a-taggen
                 li = document.createElement("li");
                 a = document.createElement("a");
+                a.appendChild(pics);
+                td.appendChild(a);
                 a.href = "#";
-                li.appendChild(pics);
                 td.appendChild(li);
+                
+                // Anropar turnCount
+                Memory.turnCount(count,a);
+
+
                 // Räknar upp.
                 count++;
                 
@@ -59,16 +70,18 @@ var Memory = {
    turnCount: function(c,länk){
        länk.onclick = function(){
            // Bilden får inte vara annat än startbild.
-           if (this.getElementByTagName("img")[0].setAttribute("src")!=="pics/0.png") {
-               return false;
-           }
+           if (this.getElementsByTagName("img")[0].getAttribute("src") !== "pics/0.png")
+              {
+                return false;
+               }
+
            
           //Lägger till ett nytt element i arrayen.
            Memory.array.push(länk);
            
              // Ifall längden på arrayen är 1 eller 2 så visar bilden
             if (Memory.array.length === 1 || Memory.array.length === 2) {
-                this.getElementsByTagName("img")[0].setAttribute("src", "pics/" + Memory.randomarray[c] + ".png");
+                this.getElementsByTagName("img")[0].setAttribute("src", "pics/" + Memory.randomArray[c] + ".png");
             }
             
             // Om bilderna stämmer inte överens, anropas checkCount funktionen efter en sekund.
@@ -80,7 +93,31 @@ var Memory = {
        };
    },
    
-   
+   checkCount: function(same_array){
+       // Testar om brickorna är lika så stannar de uppvända och en räknare räknas upp med ett.
+       if (same_array[0].getElementsByTagName("img")[0].getAttribute("src") === same_array[1].getElementsByTagName("img")[0].getAttribute("src")){
+           // Räknare för par.
+           Memory.countPair++;
+           //Kontrollerar räknaren kolla om spelaren har vunnit
+           // Om countPair är hälften så mycket är bildrna, så anändaren har vunnit.
+           if (Memory.countPair === ((cols*rows)/2)){
+               alert("Grattis! Du vann!\nAntal försök är: " + Memory.countTrying);
+           }
+           // Memory.array är töm.
+           Memory.array = [];
+       }
+       else{
+           // Startbild körs en gång till. 
+            same_array[0].getElementsByTagName("img")[0].setAttribute("src", "pics/0.png");
+            same_array[1].getElementsByTagName("img")[0].setAttribute("src", "pics/0.png");
+            //Arrayen är töm.
+            Memory.array = [];
+            // räknar upp ett försök
+            Memory.countTrying++;
+       }
+
+       
+   }
 };
 
 
