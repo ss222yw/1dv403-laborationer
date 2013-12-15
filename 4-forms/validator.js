0610d504e456887b065849c3ca2e09d853951986
sahib <ss222yw@student.lnu.se>
2013-12-15 18:36:06 -0500

    Testar moment 2 med alret så länge , får väl imorgon ändra det

--- 4-forms/validator.js
+++ 4-forms/validator.js
@@ -1,6 +1,10 @@
 "use strict";
 
 var validator = {
+    firstName: false,
+    lastName: false,
+    postCode: false,
+    eMail: false,
     
     init: function(){
          var form = null;
@@ -20,27 +24,47 @@ var validator = {
          
         
         firstName.onblur = function(){
+            var fName = form.elements['firstName'].value; 
+            if (!fName.match(/\S/)) {
+                alert("skriv ditt förnamn!");
+                return false;
             
+            }
         },
         
         lastName.onblur = function(){
-            
+            var lName = form.elements['lastName'].value; 
+            if (!lName.match(/\S/)) {
+                alert("skriv ditt efternamn!");
+                return false;
+            }
         },
         
         postCode.onblur = function(){
-            
+            var post = form.elements['postcode'].value;
+            if (!post.match(/^\d{5}$/ )) {
+                alert("Skriv ett gäligt post nummer det vill säga i formet 12345!");
+                return false;
+            }
         },
         
         email.onblur = function(){
-            
+            var Mail = form.elements['email'].value;
+            if (!Mail.match(/^[\w]+(\.[\w]+)*@([\w]+\.)+[a-z]{2,7}$/i)) {
+                alert("Skriv ett gäligt e-post!");
+                return false;
+            }
         },
         
         price.onblur = function(){
-            
+            //Tom!
         },
         
         buyButton.onclick = function(){
-            
+            if (validator.firstName === false || validator.lastName === false || validator.postCode === false || validator.eMail === false) {
+                alert("Du måste fylla i forumläret först inann du kan genomföra köpet!");
+                return false;
+            }
         };
         
         
