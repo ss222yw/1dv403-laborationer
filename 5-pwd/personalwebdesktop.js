a0c3885643ac4289b96e612ead2a7be81f78188a
sahib <ss222yw@student.lnu.se>
2013-12-28 11:56:08 -0500

    ny

--- 5-pwd/personalwebdesktop.js
+++ 5-pwd/personalwebdesktop.js
@@ -2,6 +2,26 @@
 var personalWebDesktop = {
     
     init : function(){
+        // skapar icon via id som ska öppna fönstret.
+        var icon = document.getElementById("icon");
+        // Skapar div tagg
+        var iconDiv = document.createElement("div");
+        // Skapar klass namn som i detta fallet kallas jag den image.
+        iconDiv.className = "iconImage";
+        // lägger div taggen till iconen.
+        icon.appendChild(iconDiv);
+        
+        var image = document.createElement("img");
+        image.setAttribute("src","icon.png");
+        image.className = "img";
+        
+        var a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.appendChild(image);
+        iconDiv.appendChild(a);
+        
+       
+        
         
     }
 };
