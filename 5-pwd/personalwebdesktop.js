9daccbf20f05d8778cd6cada275f67d5087980d0
sahib <ss222yw@student.lnu.se>
2014-01-05 16:55:29 -0500

    setAttribute för bilderna som finns i pics/thumbs

--- 5-pwd/personalwebdesktop.js
+++ 5-pwd/personalwebdesktop.js
@@ -7,7 +7,7 @@ var personalWebDesktop = {
         // skapar icon via id som ska öppna fönstret.
         var icon = document.getElementById("icon");
         
-        // Räknar antal tryck på ikonen.
+        // Räknar antal tryck på ikonen för att öppna ny fönster.
         var counter = 0;
         
         // Skapar div tagg
@@ -47,8 +47,9 @@ var personalWebDesktop = {
        a.onclick = function(){
            
            counter++;
-           // Enbart ett fönster ska kunna vara öppet samtidigt.
+           // Enbart ett fönster ska kunna vara öppet samtidigt.Hindrar att fler skapas.
            if(counter > 1){
+               
                return false;
            }
            
@@ -96,63 +97,12 @@ var personalWebDesktop = {
             // Lägg h div taggen inuti cont div taggen.
             contDiv.appendChild(hDiv);
             
-            // Skapar img för apple ikon som lägger i header.
-            var hIcon = document.createElement("img");
-            
-            //Skapar klass.
-            hIcon.className = "headIcon";
-            
-            // Läser bilden från filen.
-            hIcon.setAttribute("src","Apple1.png");
-            
-            // Lägg h icon inuti h div taggen.
-            hDiv.appendChild(hIcon);
-        
+           
             // Skapar en text .
             var text = document.createTextNode("Image Viewer");
             // Lägg texten inuti h div taggen.
             hDiv.appendChild(text);
             
-            
-            
-            // Skapar div taggen.
-            var bDiv = document.createElement("div");
-            
-            // Skapar klass.
-            bDiv.className = "bDiv";
-            
-            //Lägg b div taggen inuti cont div taggen.
-            contDiv.appendChild(bDiv);
-            
-            //Skapar div taggen.
-            var cDiv = document.createElement("div");
-            
-            // Skapar klass.
-            cDiv.className = "cDiv";
-            
-            // Lägg c div taggen inuti cont div taggen.
-            contDiv.appendChild(cDiv);
-            
-            // Skapar text till ajax bilden.
-            var textLoad = document.createTextNode("Laddar");
-            
-            // Lägg textLoad inuti cont div taggen.
-            contDiv.appendChild(textLoad);
-            
-            // Skapar img till ajax.
-            var imageLoad = document.createElement("img");
-            
-            //Läser bilden från filen.
-            imageLoad.setAttribute("src","ajax-loader.gif");
-            
-            //Skapar title till ajax bilden.
-            imageLoad.title ="Laddar!";
-            
-            //Skapar klass till ajax bilden.
-            imageLoad.className = "imgload";
-            
-            // Lägg imageLoad inuti b div taggen.
-            bDiv.appendChild(imageLoad);
             
             
             
@@ -166,12 +116,170 @@ var personalWebDesktop = {
                 //Lägger counter med 0 igen för att kunna öppna ny fönster igen efter att man stängt den.
                 counter = 0;
             };
+                        
+            
+           
+            
+            //Skapar div taggen.
+            var cDiv = document.createElement("div");
+            
+            // Skapar klass.
+            cDiv.className = "cDiv";
+            
+            // Lägg c div taggen inuti cont div taggen.
+            contDiv.appendChild(cDiv);
             
             
-       };
-        
-        
-    }
-};
+            
+             // Skapar div taggen.
+            var bDiv = document.createElement("div");
+            
+            // Skapar klass.
+            bDiv.className = "bDiv";
+            
+            //Lägg b div taggen inuti cont div taggen.
+            contDiv.appendChild(bDiv);
+            
+        // Skapar img för apple ikon som lägger i header.
+            var hIcon = document.createElement("img");
+            
+            //Skapar klass.
+            hIcon.className = "headIcon";
+            
+            // Läser bilden från filen.
+            hIcon.setAttribute("src","Apple1.png");
+            
+            //Skapar title.
+            hIcon.title = "Image Viewer ikonen";
+            
+            // Lägg h icon inuti h div taggen.
+            hDiv.appendChild(hIcon);
 
+
+            // Skapar img till ajax.
+            var imageLoad = document.createElement("img");
+            
+            //Läser bilden från filen.
+            imageLoad.setAttribute("src","ajax-loader.gif");
+            
+            //Skapar title till ajax bilden.
+            imageLoad.title ="Laddar.......";
+            
+            //Skapar klass till ajax bilden.
+            imageLoad.className = "imgload";
+            
+            // Lägg imageLoad inuti b div taggen.
+            bDiv.appendChild(imageLoad);
+            
+              //Skapar ny XHR objekt.
+       var url = " http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/ ";
+
+       
+       //En function som skickar url och callback.
+        new AjaxCon(url, function(data) {
+          // Parse json , spara jason.
+          var jasonImages = JSON.parse(data);
+          var width = 0;
+          var height = 0;
+          var i = null;
+
+
+          for(i=0; i < jasonImages.length; i++){
+              
+              var ajaxImageDiv = document.createElement("div");
+              
+              ajaxImageDiv.className ="ajax";
+              
+              contDiv.appendChild(ajaxImageDiv);
+              // Hitta största bredd.
+              if(jasonImages[i].thumbWidth > width){
+                  
+                  width = jasonImages[i].thumbWidth;
+              }
+              // Hitta största högt.
+              if(jasonImages[i].thumbHeight > height){
+                  
+                  height = jasonImages[i].thumbHeight;
+              }
+              
+              var ajaxImages = document.createElement("img");
+              
+              ajaxImages.setAttribute("src",jasonImages[i].thumbURL);
+              
+              var ajaxImagesLinks = document.createElement("a");
+              
+              ajaxImagesLinks.setAttribute("href","#");
+              
+              ajaxImagesLinks.appendChild(ajaxImages);
+              
+              ajaxImageDiv.appendChild(ajaxImagesLinks);
+              
+
+          }
+ 
+          //Tar bort laddning när anropet är färdigt.
+          bDiv.removeChild(imageLoad);
+        });
+     return false;
+
+
+         };
+        
+    },
+    
+ 
+};
+   function AjaxCon(url, callback) {
+        
+    var READY_STATE_UNITIALIZED = 0;
+    
+    var READY_STATE_OPENED = 1;
+    
+    var READY_STATE_SENT = 2;
+    
+    var READY_STATE_LOADING = 3;
+    
+    var READY_STATE_COMPLETE = 4;
+
+    //Pickar (this) på getXHR funktionen.
+    var xhr = this.getXHR();
+    xhr.onreadystatechange = function () {
+
+        if (xhr.readyState === READY_STATE_COMPLETE) {
+            
+             //Ifall xhr är lika med 200 eller större än 200 eller mindre än 300 eller lika med 304 picka på callback.
+            if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+            
+                callback(xhr.responseText);
+                console.log(xhr);
+            }
+
+            else
+            {
+                console.log("läsfel, status:" + xhr.status);  
+            }
+        }
+
+    };
+
+   xhr.open("get", url, true);
+   
+
+    xhr.send(null);
+}
+
+   AjaxCon.prototype.getXHR = function () {
+    var xhr = null;
+        try {
+        xhr = new XMLHttpRequest();        
+    } catch (error){
+        
+        }
+
+    return xhr;
+};
+    
+    
+    
+    
 window.onload = personalWebDesktop.init;
